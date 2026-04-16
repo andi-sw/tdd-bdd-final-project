e117fc3e19f7b5c5aa0aca37834d12c772dd50ba
Andreas Luksch <andreas.luksch@outlook.de>
2026-04-16 07:27:49 -0400

    fix route test for update

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -262,9 +262,24 @@ class TestProductRoutes(TestCase):
     # ----------------------------------------------------------
     def test_update_product(self):
         """It should Update a single Product"""
-        # get the id of a product
-        test_product = self._create_products(1)[0]
+        # create product
+        test_product = ProductFactory()
+        logging.debug("Test Product: %s", test_product.serialize())
+        response = self.client.post(BASE_URL, json=test_product.serialize())
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+        # Check the data is correct
+        new_product = response.get_json()
+        self.assertEqual(new_product["name"], test_product.name)
+        self.assertEqual(new_product["description"], test_product.description)
+        self.assertEqual(Decimal(new_product["price"]), test_product.price)
+        self.assertEqual(new_product["available"], test_product.available)
+        self.assertEqual(new_product["category"], test_product.category.name)
+        self.assertNotEqual(new_product["id"], None)
+
         # update product by id
+        # enter actual id based on response
+        test_product.id = new_product["id"]
         test_product.name = "new name"
         response = self.client.put(f"{BASE_URL}/{test_product.id}", json=test_product.serialize())
         self.assertEqual(response.status_code, status.HTTP_200_OK)
